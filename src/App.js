8f9f37f66657ca2c5a717d3256727d4cd5d0e9e6
Sonal Patel <sonal@justgiving.com>
2018-05-08 11:01:44 +0100

    added header and footer compoents to App.js

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import Footer from "./components/Footer.js";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/*change the header to my own component and add it here*/}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
+        <Header />
 
         <p className="testing-sass">MY JG APP</p>
+
+        <Footer />
       </div>
     );
   }
