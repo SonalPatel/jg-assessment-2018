f95768822b133ab9e06c2925c55ff4fd54cba5ee
Sonal Patel <sonal@justgiving.com>
2018-05-08 20:20:34 +0100

    sass styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./styles/App.css";
 import Header from "./components/Header.js";
 import WrapContainer from "./containers/WrapContainer.js";
@@ -10,9 +9,7 @@ class App extends Component {
     return (
       <div>
         <Header />
-
         <WrapContainer />
-
         <Footer />
       </div>
     );
