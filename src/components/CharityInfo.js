c6d89b866401a9fca5871482b733f4f21ee85b2e
Sonal Patel <sonal@justgiving.com>
2018-05-08 11:32:50 +0100

    set up CharityInfo and Donations components

---
+++ src/components/CharityInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class CharityInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Main page heading </h1>
+        <p className="testing-sass-again">
+          {" "}
+          charity info goes here from the API
+        </p>
+        <p>charity info lorem ipsum dolor sit amet</p>
+        <p>charity info lorem ipsum dolor sit amet</p>
+      </div>
+    );
+  }
+}
+
+export default CharityInfo;
---
+++ src/components/Donations.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Donations extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>charity donations go here </h2>
+        <p className="testing-sass-again"> make this a list</p>
+      </div>
+    );
+  }
+}
+
+export default Donations;
