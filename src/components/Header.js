a2339fcf65525e2dd763ac6beda0457fca17c05a
Sonal Patel <sonal@justgiving.com>
2018-05-08 10:55:10 +0100

    created the start of the header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> The header goes here</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
--- src/containers/WrapContainer.js
+++ src/containers/WrapContainer.js
@@ -6,9 +6,11 @@ class WrapContainer extends React.Component {
   render() {
     return (
       <div className="x">
-        <div className="y">MyComponent 1 goes here</div>
+        <div className="y">
+          MyComponent for charity (oxfam: 13441) info goeshere
+        </div>
 
-        <div className="z">MyComponent 2 goes here</div>
+        <div className="z">MyComponent for donations goes here</div>
       </div>
     );
   }
