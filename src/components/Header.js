9487fc072e12552861ed9354f114495f9c51b628
Sonal Patel <sonal@justgiving.com>
2018-05-08 11:10:50 +0100

    skip links added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,25 @@ class Header extends React.Component {
   render() {
     return (
       <div>
+        <span className="off-screen">Skip Links</span>
+        <ul className="off-screen">
+          <li>
+            <a className="skip-link" href="#content">
+              Skip to main content
+            </a>
+          </li>
+          <li>
+            <a className="skip-link" href="#latest-donations">
+              Skip to latest donations
+            </a>
+          </li>
+          <li>
+            <a className="skip-link" href="#footer-content">
+              Skip to footer
+            </a>
+          </li>
+        </ul>
+
         <p className="testing-sass"> The header goes here</p>
       </div>
     );
