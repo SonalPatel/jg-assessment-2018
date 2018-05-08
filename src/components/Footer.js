ce42d3751389a9d0929bf11136f2274ec7fc1569
Sonal Patel <sonal@justgiving.com>
2018-05-08 10:59:56 +0100

    added start of footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div>
+        <p className="testing-sass-again"> The footer goes here</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
